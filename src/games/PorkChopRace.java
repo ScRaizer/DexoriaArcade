d2aaa0a570924f08150b222cc3a743708c0581e1
lewysDavies <lewys.davies@btinternet.com>
2014-07-08 15:03:32 +0100

    changes
    changes

--- src/games/PorkChopRace.java
+++ src/games/PorkChopRace.java
@@ -5,19 +5,28 @@ import java.util.List;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Sound;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
 
+import com.lewys.arcade.Main;
 import com.lewys.arcade.util.particles.ParticleEffect;
+import com.sk89q.minecraft.util.commands.ChatColor;
 
 public class PorkChopRace implements Listener
 {
-	List<Entity> pigs = new ArrayList<Entity>();
+	static List<Entity> pigs = new ArrayList<Entity>();
+	static boolean Event = false;
+	static boolean canMove = false;
 	
-	public void doPorkChopRace()
-	{
+	public static void doPorkChopRace()
+	{	
+		Event = true;
+		
 		Location loc = new Location(Bukkit.getWorld("world"), -120.5, 7.5,1410.5);
 		
 		for(Player p : Bukkit.getOnlinePlayers())
@@ -26,14 +35,37 @@ public class PorkChopRace implements Listener
 			ParticleEffect.FLAME.display(loc, 0.3f, 1f, 0.3f, 0.1f, 20);
 			
 			loc.setX(loc.getX() -2);
+			
+			p.playSound(loc, Sound.FALL_BIG, 2.0f, 2.0f);
+		}	
+		
+		Bukkit.broadcastMessage(ChatColor.GREEN + "----" + ChatColor.WHITE + "ProkChopRace" + ChatColor.GREEN + "----");
+		Bukkit.broadcastMessage(ChatColor.WHITE + "Race to the pen and kill 3 pigs");
+		Bukkit.broadcastMessage(ChatColor.WHITE + "Then race back to the camp with the pork!");
+		Bukkit.broadcastMessage(ChatColor.WHITE + "First one back with the bacon wins");
+		Bukkit.broadcastMessage(ChatColor.AQUA + "Good luck!");
+		
+		Bukkit.getScheduler().scheduleSyncDelayedTask(Main.instance, new Runnable(){
 
-		}		
+			@Override
+			public void run() {
+				spawnPigs();
+				canMove = true;
+				Bukkit.broadcastMessage(ChatColor.GREEN + "" + ChatColor.BOLD + "GO");
+				
+				for(Player p : Bukkit.getOnlinePlayers())
+				{
+				p.playSound(p.getLocation(), Sound.PIG_DEATH, 2.0f, 2.0f);
+				}
+			}
+			
+		}, 60);
 	}
 	
 	/* 
 	 * This will spawn 30 pigs in the pen
 	 * */
-	public void spawnPigs()
+	public static void spawnPigs()
 	{
 		for(int i = 0; i <30; i++)
 		{
@@ -43,7 +75,37 @@ public class PorkChopRace implements Listener
 			
 			Location loc = new Location(Bukkit.getWorld("world"), x,y,z);
 			
-			Bukkit.getWorld("world").spawnEntity(loc, EntityType.PIG);
+			Entity ent = Bukkit.getWorld("world").spawnEntity(loc, EntityType.PIG);
+			pigs.add(ent);
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/* 
+	 * Events
+	 * */
+	
+	@EventHandler
+	public void PlayerMove(PlayerMoveEvent e)
+	{
+		if(Event == true)
+		{
+			if(canMove == false)
+			{
+				e.setCancelled(true);
+			}
 		}
 	}
 }
